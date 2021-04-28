cbbe8a11c9317c164ac828e5bfcdb82890933cec
Andrew Loveless <andrew@andrewloveless.net>
2021-04-27 20:44:51 -0400

    added table status check, table assignment to waitstaff, and menu parsing

--- swePrototype/src/prototype/Main.java
+++ swePrototype/src/prototype/Main.java
@@ -3,117 +3,212 @@ package prototype;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-import javafx.scene.shape.*;
-import javafx.scene.paint.Color;
-import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main extends Application {
 
+    Label tableLabel = new Label();
+    Label seat1Label = new Label();
+    Label seat2Label = new Label();
+    Label seat3Label = new Label();
+    Label seat4Label = new Label();
+    Button seat1Button = new Button();
+    Button seat2Button = new Button();
+    Button seat3Button = new Button();
+    Button seat4Button = new Button();
+
+    Button tableButtons[][] = new Button[5][6];
+
+    Table[] tables = new Table[30];
+    private String username;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
+        for (int i = 0; i < 30; i++)
+            tables[i] = new Table(i, 0);
+
         String waiterTables[] = new String[5];
         waiterTables[0] = "0,0";
-        waiterTables[1] = "0,1";
-        waiterTables[2] = "0,2";
-        waiterTables[3] = "1,0";
-        waiterTables[4] = "1,1";
+        waiterTables[1] = "0,2";
+        waiterTables[2] = "2,0";
+        waiterTables[3] = "2,2";
+        waiterTables[4] = "3,1";
 
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("J's Corner Restaurant Prototype");
 
+
         //Login screen root
-        GridPane root = new GridPane();
+        GridPane loginPane = new GridPane();
 
         //Main application root
-        GridPane root2 = new GridPane();
+        GridPane mainPane = new GridPane();
+
+
+        loginPane.setPadding(new Insets(10, 10, 10, 10));
 
         //Username label
         Label userLabel = new Label();
         userLabel.setText("Username");
-        root.add(userLabel,0, 0, 1, 1);
+        loginPane.add(userLabel,0, 0, 1, 1);
 
         //Password label
         Label passLabel = new Label();
         passLabel.setText("Password");
-        root.add(passLabel,1, 0, 1, 1);
+        loginPane.add(passLabel,1, 0, 1, 1);
 
         //Label for incorrect password
         Label errorLabel = new Label();
         errorLabel.setText("");
-        root.add(errorLabel, 1, 2, 1, 1);
+        loginPane.add(errorLabel, 1, 3, 1, 1);
 
         //Username entry field
         TextField userEntry = new TextField();
-        root.add(userEntry, 0, 1, 1, 1);
+        loginPane.add(userEntry, 0, 1, 1, 1);
 
         //Password entry field
-        TextField passEntry = new TextField();
-        root.add(passEntry, 1, 1, 1, 1);
+        PasswordField passEntry = new PasswordField();
+        loginPane.add(passEntry, 1, 1, 1, 1);
 
         //Sign in button
         Button signIn = new Button();
         signIn.setText("Sign In");
         signIn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                System.out.println("Test Complete");
-                boolean validAttempt = signIn(userEntry.getText(), passEntry.getText());
+                username = userEntry.getText();
+
+                boolean validAttempt = signIn(username, passEntry.getText());
 
                 if(validAttempt) {
-                    System.out.println("Success");
-                    primaryStage.setScene(new Scene(root2, 1280, 960));
+                    System.out.println("Logged in as " + username);
+                    assignTables();
+                    primaryStage.setScene(new Scene(mainPane, 1280, 960));
                     primaryStage.show();
                 }
 
                 else {
-                    errorLabel.setText("The username or password is incorrect");
+                    errorLabel.setText("Incorrect login");
                 }
             }
         });
-        root.add(signIn, 0, 3, 1, 1);
+        loginPane.add(signIn, 0, 3, 1, 1);
 
-        root2.setHgap(10);
-        root2.setVgap(10);
+        GridPane addOrderPane = new GridPane();
+        addOrderPane.setPrefWidth(400);
+        addOrderPane.setPrefHeight(400);
+        addOrderPane.setLayoutX(640);
+        addOrderPane.setLayoutY(320);
 
+
+
+        tableLabel.setText("Table");
+        seat1Label.setText("Seat 1 Order");
+        seat2Label.setText("Seat 2 Order");
+        seat3Label.setText("Seat 3 Order");
+        seat4Label.setText("Seat 4 Order");
+        seat1Button.setText("Add");
+        seat2Button.setText("Add");
+        seat3Button.setText("Add");
+        seat4Button.setText("Add");
+
+        seat1Button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                addOrderButton(0);
+            }
+        });
+        seat2Button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                addOrderButton(1);
+            }
+        });
+        seat3Button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                addOrderButton(2);
+
+            }
+        });
+        seat4Button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                addOrderButton(3);
+            }
+        });
+
+        tableLabel.setVisible(false);
+        seat1Label.setVisible(false);
+        seat2Label.setVisible(false);
+        seat3Label.setVisible(false);
+        seat4Label.setVisible(false);
+        seat1Button.setVisible(false);
+        seat2Button.setVisible(false);
+        seat3Button.setVisible(false);
+        seat4Button.setVisible(false);
+
+        addOrderPane.add(tableLabel, 0, 0);
+        addOrderPane.add(seat1Label, 0, 1);
+        addOrderPane.add(seat2Label, 0, 2);
+        addOrderPane.add(seat3Label, 0, 3);
+        addOrderPane.add(seat4Label, 0, 4);
+        addOrderPane.add(seat1Button, 2, 1);
+        addOrderPane.add(seat2Button, 2, 2);
+        addOrderPane.add(seat3Button, 2, 3);
+        addOrderPane.add(seat4Button, 2, 4);
+
+        mainPane.add(addOrderPane, 0, 6, 6, 1);
+
+
+        loginPane.setVgap(5);
+        loginPane.setHgap(5);
+        mainPane.setHgap(10);
+        mainPane.setVgap(10);
+        addOrderPane.setHgap(10);
+        addOrderPane.setVgap(10);
 
         Line divider = new Line(0, 0, 0, 960);
-        root2.add(divider, 6, 0, 1, 6);
+        mainPane.add(divider, 6, 0, 1, 6);
 
-        Button rectArray[][] = new Button[5][6];
-        int count = 1;
+        int count = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 6; j++) {
 
-                rectArray[i][j] = new Button(String.valueOf(count));
-                rectArray[i][j].setStyle("-fx-background-color: #ffffff; -fx-border-color: #000000");
-                rectArray[i][j].setPrefHeight(150);
-                rectArray[i][j].setPrefWidth(150);
-                //rectArray[i][j].setStroke(Color.BLACK);
-                root2.add(rectArray[i][j], j, i, 1, 1);
+                tableButtons[i][j] = new Button(String.valueOf(count));
+                tableButtons[i][j].setStyle("-fx-background-color: #ffffff; -fx-border-color: #000000");
+                tableButtons[i][j].setPrefHeight(150);
+                tableButtons[i][j].setPrefWidth(150);
+                int number = i * 6 + j;
+                tableButtons[i][j].setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent actionEvent) {
+                        updateTableStatus(number);
+                        tableLabel.setVisible(true);
+                        seat1Label.setVisible(true);
+                        seat2Label.setVisible(true);
+                        seat3Label.setVisible(true);
+                        seat4Label.setVisible(true);
+                        seat1Button.setVisible(true);
+                        seat2Button.setVisible(true);
+                        seat3Button.setVisible(true);
+                        seat4Button.setVisible(true);
+                    }
+                });
+                mainPane.add(tableButtons[i][j], j, i, 1, 1);
                 count++;
             }
         }
 
-        for (int i = 0; i < waiterTables.length; i++) {
-            String parsedTables[] = waiterTables[i].split(",");
-            int waiterTableLocation[] = new int[2];
-            waiterTableLocation[0] = Integer.parseInt(parsedTables[0]);
-            waiterTableLocation[1] = Integer.parseInt(parsedTables[1]);
-            rectArray[waiterTableLocation[0]][waiterTableLocation[1]].setStyle("-fx-background-color: #00ff00; ");
-        }
-
-
-        primaryStage.setScene(new Scene(root2, 1280, 960));
+        primaryStage.setScene(new Scene(loginPane, 360, 100));
+        //primaryStage.setScene(new Scene(mainPane, 1280, 960));
         primaryStage.show();
 
     }
@@ -122,9 +217,9 @@ public class Main extends Application {
     public boolean signIn(String username, String password) {
         try {
             //replace this string with filepath on your computer
-            String filepath = "C:\\Users\\Alex\\Desktop\\swePrototype\\src\\sample\\loginInfo.txt";
-            File employeeInfo = new File(filepath);
-            Scanner scan = new Scanner(employeeInfo);
+            String filepath = "../../../src/prototype/users.csv";
+            File userDB = new File(filepath);
+            Scanner scan = new Scanner(userDB);
 
             //Keeps track of if a login matches a stored value
             boolean validLogin = false;
@@ -135,11 +230,9 @@ public class Main extends Application {
                 String[] login = data.split(",");
 
                 //If username and password are the same, login is set to valid
-                if (username.equals(login[0])) {
-                    if (password.equals(login[1])) {
+                if (username.equals(login[0]) && password.equals(login[1])) {
                         validLogin = true;
                         break;
-                    }
                 }
             }
             if (validLogin) {
@@ -158,6 +251,50 @@ public class Main extends Application {
         return false;
     }
 
+    public void addOrderButton(int seat)
+    {
+        int tableNumber = Integer.parseInt(tableLabel.getText().split(" ")[1]);
+        tables[tableNumber].setOrder(seat, takeOrder());
+        updateTableStatus(tableNumber);
+    }
+
+    public Order takeOrder() {
+        Order order = new Order();
+        //Take the order
+        order.addItem(15);
+        return order;
+    }
+
+    public void updateTableStatus(int number)
+    {
+        Order[] orders = new Order[4];
+        for (int i = 0; i < 4; i++)
+            orders[i] = tables[number].getOrder(i);
+
+        tableLabel.setText("Table " + number);
+        seat1Label.setText("Seat 1 Order: " + (orders[0] != null ? orders[0] : ""));
+        seat2Label.setText("Seat 2 Order: " + (orders[1] != null ? orders[1] : ""));
+        seat3Label.setText("Seat 3 Order: " + (orders[2] != null ? orders[2] : ""));
+        seat4Label.setText("Seat 4 Order: " + (orders[3] != null ? orders[3] : ""));
+    }
+
+    public void assignTables() {
+        try {
+            String filepath = "../../../src/prototype/tables.csv";
+            File tableDB = new File(filepath);
+            Scanner scan = new Scanner(tableDB);
+            while (scan.hasNextLine()) {
+                String data = scan.nextLine();
+                String[] assignment = data.split(",");
+                if (assignment[0].equals(username))
+                    tableButtons[Integer.parseInt(assignment[1])][Integer.parseInt(assignment[2])]
+                            .setStyle("-fx-background-color: #00ff00; -fx-border-color: #000000");
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
