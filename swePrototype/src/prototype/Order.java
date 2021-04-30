a1af0dd32e456ec2333ff6394988ce14ad041abb
Andrew Loveless <andrew@andrewloveless.net>
2021-04-30 11:16:28 -0400

    Commented and cleaned up code

--- swePrototype/src/prototype/Order.java
+++ swePrototype/src/prototype/Order.java
@@ -2,23 +2,43 @@ package prototype;
 
 import java.util.ArrayList;
 
+/**
+ * Order --- Stores a list of individual menu items.
+ * @author Andrew Loveless
+ * @version 1.0
+ */
 public class Order
 {
     private ArrayList<MenuItem> items = new ArrayList<MenuItem>();
 
+    /**
+     * Create an empty order.
+     */
     public Order() {
         this.items = items;
     }
 
+    /**
+     * Create an order and add a menu item to it.
+     * @param id The menu item to add to the order
+     */
     public Order(int id) {
         this.items = items;
         addItem(id);
     }
 
+    /**
+     * Return the list of menu items on the order.
+     * @return items The arraylist of menu items
+     */
     public ArrayList<MenuItem> getItems() {
         return items;
     }
 
+    /**
+     * Calculate the total cost of the items on the order.
+     * @return total The total cost of the order
+     */
     public int getTotal() {
         int total = 0;
         for (MenuItem item : items)
@@ -28,20 +48,37 @@ public class Order
         return total;
     }
 
+    /**
+     * Add a menu item to the order.
+     * @param id The menu item to add to the order
+     */
     public void addItem(int id) {
         items.add(new MenuItem(id));
     }
 
+    /**
+     * Get the name of the menu item at the specified index.
+     * @param index The index of the menu item to check
+     * @return The name of the menu item at the index
+     */
     public String get(int index)
     {
         return items.get(index).getName();
     }
 
+    /**
+     * Return the number of items on the order.
+     * @return The number of items on the order
+     */
     public int size()
     {
         return items.size();
     }
 
+    /**
+     * Print the order as a list of its menu items and total cost.
+     * @return The total cost of the order followed by each item on the order
+     */
     @Override
     public String toString() {
         String output = "($" + getTotal() + ") ";
--- swePrototype/src/prototype/Table.java
+++ swePrototype/src/prototype/Table.java
@@ -1,52 +1,73 @@
 package prototype;
 
+/**
+ * Table --- Stores table information such as number, status, and orders at each seat.
+ * @author Andrew Loveless
+ * @version 1.0
+ */
 public class Table
 {
 
-    //Status [0 - Open] [1 - Occupied] [2 - Dirty]
+
     private int number, status;
     private Order[] orders = new Order[4];
 
+    /**
+     * Create the table object.
+     * @param number
+     * @param status
+     */
     public Table(int number, int status) {
         this.number = number;
         this.status = status;
 
+        // Initialize the orders for each seat as empty
         for (int i = 0; i < orders.length; i++) {
             orders[i] = new Order();
         }
     }
 
+    /**
+     * Returns if the table is open (0), occupied (1), or dirty (2).
+     * @return status The status of the table
+     */
     public int getStatus() {
         return status;
     }
 
+    /**
+     * Returns the table's number.
+     * @return number The table's number
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Returns the order at the specified seat index.
+     * @param index The seat to get the order from
+     * @return orders[index] The order from the checked seat
+     */
     public Order getOrder(int index) {
         return orders[index];
     }
 
+    /**
+     * Set the status of the table to either open (0), occupied (1), or dirty (2).
+     * @param newStatus The new status of the table
+     */
     public void setStatus(int newStatus) {
-
         status = newStatus;
     }
 
+    /**
+     * Add a menu item to the end of a seat's current order.
+     * @param seat The seat which is being added to
+     * @param item The menu item to add to the order
+     */
     public void addToOrder(int seat, int item) {
         Order order = getOrder(seat);
         order.addItem(item);
-        setOrder(seat, order);
+        orders[seat] = order;
     }
-
-    public void setNumber(int newNumber) {
-
-        number = newNumber;
-    }
-
-    public void setOrder(int index, Order newOrder) {
-
-        orders[index] = newOrder;
-    }
-
 }
